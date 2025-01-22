56d427ec1c37399b54ee7f38931cf42286f00242
Shadowress <JamesKanKH@gmail.com>
2025-01-22 18:44:39 +0800

    initialize task and task_manager classes

--- task.py
+++ task.py
@@ -1,3 +1,20 @@
-class Task:
-    ...
+import datetime
 
+
+class Task:
+
+    def __init__(self, title: str, description: str, due_date: datetime, status):  #todo status would be an enum
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.status = status
+
+    def __str__(self):
+        ...
+
+    def to_dict(self):
+        ... #todo convert task for saving to json or csv
+
+    @classmethod
+    def from_dict(cls):
+        ... #todo THE USE OF THIS METHOD IS TO BE DEFINED
---
+++ task_manager.py
@@ -0,0 +1,25 @@
+import task
+
+
+class TaskManager:
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load_tasks(self):
+        ...
+
+    def save_tasks(self):
+        ...
+
+    def add_task(self):
+        ...
+
+    def delete_task(self):
+        ...
+
+    def edit_task(self):
+        ...
+
+    def find_task_by_title(self):
+        ...
